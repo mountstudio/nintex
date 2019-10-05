e4340bd0b978c2752028164044015acec3c15ca6
Uki7991 <tilek.kubanov@gmail.com>
2019-10-05 15:55:43 +0600

    Start

---
+++ app/Comment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
