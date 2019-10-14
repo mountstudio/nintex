4077d5ab2ad1e1a9ad8b87aec109dd88f5b4a986
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-14 20:46:56 +0600

    fx footer

--- resources/views/partials/socialnetworks.blade.php
+++ resources/views/partials/socialnetworks.blade.php
@@ -1,22 +1,18 @@
 <div class="pb-4">
     <a href="#" class="text-muted" title="Ссылка на страницу в Facebook">
-        <i class="fab fa-facebook mr-3 fa-lg nav-scale"></i>
-        <img src="" alt="">
+{{--        <i class="fab fa-facebook mr-3 fa-lg nav-scale"></i>--}}
+        <img src="icons/whatsap_icon.svg"  alt="ссылка на whatsap">
     </a>
     <a href="#" class="text-muted" title="Ссылка на youtube">
-        <i class="fab fa-youtube mr-3 fa-lg nav-scale"></i>
-        <img src="" alt="">
+        <img src="icons/vk_icon.svg" class="mx-3" alt="ссылка на vk">
     </a>
     <a href="#" class="text-muted" title="Ссылка на instagram">
-        <i class="fab fa-instagram mr-3 fa-lg nav-scale"></i>
-        <img src="" alt="">
+        <img src="icons/telegram_icon.svg" alt="ссылка на telegram">
     </a>
     <a href="#" class="text-muted" title="Ссылка на vk">
-        <i class="fab fa-vk mr-3 fa-lg nav-scale"></i>
-        <img src="" alt="">
+        <img src="icons/facebook_icon.svg" class="mx-3" alt="Ссылка на facebook">
     </a>
     <a href="#" class="text-muted">
-        <i class="fab fa-weixin mr-3 fa-lg nav-scale" title="Ссылка на Wechat"></i>
-        <img src="insta_icon.svg" alt="">
+        <img src="icons/insta_icon.svg" alt="Ссылка на instagram">
     </a>
 </div>
