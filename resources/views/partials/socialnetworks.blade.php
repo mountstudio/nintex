b5492b8318ecd2e178ca57c61ac75dc48410b3ce
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-17 18:53:08 +0600

    fx

--- resources/views/partials/socialnetworks.blade.php
+++ resources/views/partials/socialnetworks.blade.php
@@ -1,18 +1,17 @@
 <div class="pb-4">
-    <a href="#" class="text-muted" title="Ссылка на страницу в Facebook">
-{{--        <i class="fab fa-facebook mr-3 fa-lg nav-scale"></i>--}}
+    <a href="https://api.whatsapp.com/send?phone=996555449342" class="text-muted" title="Ссылка на страницу в Facebook">
         <img src="icons/whatsap_icon.svg"  alt="ссылка на whatsap">
     </a>
-    <a href="#" class="text-muted" title="Ссылка на youtube">
+    <a href="https://vk.com/id17653717" class="text-muted" title="Ссылка на vk">
         <img src="icons/vk_icon.svg" class="mx-3" alt="ссылка на vk">
     </a>
-    <a href="#" class="text-muted" title="Ссылка на instagram">
+    <a href="https://t.me/joinchat/AAAAAFHPZo4S9LPeCH442g" class="text-muted" title="Ссылка на telegram">
         <img src="icons/telegram_icon.svg" alt="ссылка на telegram">
     </a>
-    <a href="#" class="text-muted" title="Ссылка на vk">
+    <a href="https://www.facebook.com/Nintex-112450370165426/" class="text-muted" title="Ссылка на facebook">
         <img src="icons/facebook_icon.svg" class="mx-3" alt="Ссылка на facebook">
     </a>
-    <a href="#" class="text-muted">
+    <a href="https://www.instagram.com/nintexkg/?igshid=k2gtwlzqnan1" class="text-muted">
         <img src="icons/insta_icon.svg" alt="Ссылка на instagram">
     </a>
 </div>
