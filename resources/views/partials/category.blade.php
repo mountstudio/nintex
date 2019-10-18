3ec00fa510db136d4d5b709351207122e3653217
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-18 19:02:53 +0600

    fx

--- resources/views/partials/category.blade.php
+++ resources/views/partials/category.blade.php
@@ -5,22 +5,22 @@
                 <h1>Категории</h1>
             </div>
             <div class="card-columns">
-                <div class="card card_box_shadow ">
-                    <div class="card-body">
+                <div class="card card_box_shadow">
+                    <div class="card-body  box_sh">
                         <a href="">
                             <img src="img/pre.svg" class="card-img-top" alt="...">
                         </a>
                     </div>
                 </div>
                 <div class="card card_box_shadow">
-                    <div class="card-body">
+                    <div class="card-body  box_sh">
                         <a href="">
                             <img src="img/pre.svg" class="card-img-top" alt="...">
                         </a>
                     </div>
                 </div>
                 <div class="card card_box_shadow">
-                    <div class="card-body">
+                    <div class="card-body  box_sh">
                         <a href="">
                             <img src="img/pre.svg" class="card-img-top" alt="...">
                         </a>
