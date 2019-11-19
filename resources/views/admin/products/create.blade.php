eae584bf24ac3cd2500dbe0ff990c6b77a58848b
Azret <azret1jusuev@gmail.com>
2019-11-19 17:52:11 +0600

    add video!

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -53,11 +53,55 @@
                 <label>Выберите фото</label>
                 <input type="file" name="video">
             </div>
+            <div class="col-6">
+                <span class="multi-select"></span>
+            </div>
+
         </div>
-        <button type="submit" title="{{ __('Создать') }}" class="bt n btn-success">{{ __('Создать') }}</button>
+
+        <button type="submit" title="{{ __('Создать') }}" class="btn btn-success">{{ __('Создать') }}</button>
     </form>
 @endsection
 
+
+
+@push('styles')
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/multiselect.css') }}">
+@endpush
+
 @push('scripts')
     <script src="{{asset('js/field.js')}}"></script>
+    <script src="{{ asset('js/bundle.min.js') }}"></script>
+    <script>
+        var multi = new SelectPure(".multi-select", {
+            options: [
+                {
+                    label: "S",
+                    value: "S",
+                },
+                {
+                    label: "M",
+                    value: "M",
+                },
+                {
+                    label: "L",
+                    value: "L",
+                },
+                {
+                    label: "XL",
+                    value: "XL",
+                },
+                {
+                    label: "XXL",
+                    value: "XXL",
+                },
+            ],
+            value: ["L", "S"],
+            multiple: true,
+            icon: "fa fa-times",
+            onChange: value => { console.log(value); },
+        });
+    </script>
 @endpush
