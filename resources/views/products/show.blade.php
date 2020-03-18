9ad443a6801ba946f6f6b2c8dfb8d3d8f7c4c3d2
Azret <azret1jusuev@gmail.com>
2020-03-18 11:18:15 +0600

    new version

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -342,54 +342,54 @@
                     {{--                            </div>--}}
                     {{--                        </div>--}}
                     <!--Pagination -->
-                        <nav class="d-flex justify-content-center mt-5">
-                            <ul class="pagination pg-blue mb-0">
+{{--                        <nav class="d-flex justify-content-center mt-5">--}}
+{{--                            <ul class="pagination pg-blue mb-0">--}}
 
-                                <!--First-->
-                                <li class="page-item disabled">
-                                    <a class="page-link waves-effect waves-effect">First</a>
-                                </li>
+{{--                                <!--First-->--}}
+{{--                                <li class="page-item disabled">--}}
+{{--                                    <a class="page-link waves-effect waves-effect">First</a>--}}
+{{--                                </li>--}}
 
-                                <!--Arrow left-->
-                                <li class="page-item disabled">
-                                    <a class="page-link waves-effect waves-effect" aria-label="Previous">
-                                        <span aria-hidden="true">«</span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                </li>
+{{--                                <!--Arrow left-->--}}
+{{--                                <li class="page-item disabled">--}}
+{{--                                    <a class="page-link waves-effect waves-effect" aria-label="Previous">--}}
+{{--                                        <span aria-hidden="true">«</span>--}}
+{{--                                        <span class="sr-only">Previous</span>--}}
+{{--                                    </a>--}}
+{{--                                </li>--}}
 
-                                <!--Numbers-->
-                                <li class="page-item active">
-                                    <a class="page-link waves-effect waves-effect">1</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link waves-effect waves-effect">2</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link waves-effect waves-effect">3</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link waves-effect waves-effect">4</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link waves-effect waves-effect">5</a>
-                                </li>
+{{--                                <!--Numbers-->--}}
+{{--                                <li class="page-item active">--}}
+{{--                                    <a class="page-link waves-effect waves-effect">1</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="page-item">--}}
+{{--                                    <a class="page-link waves-effect waves-effect">2</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="page-item">--}}
+{{--                                    <a class="page-link waves-effect waves-effect">3</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="page-item">--}}
+{{--                                    <a class="page-link waves-effect waves-effect">4</a>--}}
+{{--                                </li>--}}
+{{--                                <li class="page-item">--}}
+{{--                                    <a class="page-link waves-effect waves-effect">5</a>--}}
+{{--                                </li>--}}
 
-                                <!--Arrow right-->
-                                <li class="page-item">
-                                    <a class="page-link waves-effect waves-effect" aria-label="Next">
-                                        <span aria-hidden="true">»</span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
-                                </li>
+{{--                                <!--Arrow right-->--}}
+{{--                                <li class="page-item">--}}
+{{--                                    <a class="page-link waves-effect waves-effect" aria-label="Next">--}}
+{{--                                        <span aria-hidden="true">»</span>--}}
+{{--                                        <span class="sr-only">Next</span>--}}
+{{--                                    </a>--}}
+{{--                                </li>--}}
 
-                                <!--Last-->
-                                <li class="page-item">
-                                    <a class="page-link waves-effect waves-effect">Last</a>
-                                </li>
+{{--                                <!--Last-->--}}
+{{--                                <li class="page-item">--}}
+{{--                                    <a class="page-link waves-effect waves-effect">Last</a>--}}
+{{--                                </li>--}}
 
-                            </ul>
-                        </nav>
+{{--                            </ul>--}}
+{{--                        </nav>--}}
                         <!--Pagination -->
 
                     </section>
