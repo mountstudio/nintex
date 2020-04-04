88dc347ea6e78c9834a2f258adc3d4a8b8a104fe
Uki7991 <tilek.kubanov@gmail.com>
2020-04-04 18:52:49 +0600

    web

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Http\Request;
 */
 Route::post('/favorite', 'Api\FavoriteController@favorite')->name('production.favorite');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
